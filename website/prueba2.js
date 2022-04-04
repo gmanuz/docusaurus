56119756e5bad0ec5ac630fc2762369ca28d7b8e
gmanuz <gmanuzc01@educantabria.es>
2022-04-04 10:55:25 +0200

    Create prueba2.js

---
+++ website/prueba2.js
@@ -0,0 +1,11 @@
+<>/**prueba con JAVA SCRIPT (PROGRAMAM DE LENGUAJE DE MARCAS) */
+    //redirecionar una pagina a otra 
+    <script type="text/javascript">
+        window.location.href= "https://teams.microsoft.com/";
+    </script>
+    //*mismo codigo lo unico que con un tiempo de X segundos */ 
+    //redirecionar una pagina con tiempo 
+    <script type="text/javascript">
+        setTimeout( function() { window.location.href = "https://teams.microsoft.com/"},5000 );
+    </script>
+</>
